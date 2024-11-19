e4750ce5f0fa7dd56a77745b6ef429c0eaf14ca8
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-11-19 14:40:32 +0530

    intial

---
+++ index.js
@@ -0,0 +1,45 @@
+// const body = document.body
+
+// const div = document.createElement('div')
+// div.innerText = 'Hello world'
+// div.textContent = 'Hello world again'
+
+// div.innerHTML = '<strong>Hello world strong</strong>'
+
+// const strong = document.createElement('strong')
+// strong.innerText = 'Hello world strong again'
+// div.append(strong)
+
+// body.append(div)
+
+
+/** remove and Attribute */
+const helloSpan = document.querySelector('#hello')
+// const worldSpan = document.querySelector('#world')
+// const div1 = document.querySelector('div1')
+
+// helloSpan.remove()
+// div1.removeChild(helloSpan)
+
+// console.log(helloSpan.title)
+// helloSpan.setAttribute('title', 'Hello again')
+// console.log(helloSpan.getAttribute('title'))
+
+// helloSpan.title = 'Hello Again'
+// console.log(helloSpan.title)
+
+// helloSpan.removeAttribute('title')
+
+// console.log(helloSpan.dataset.longName)
+
+// helloSpan.dataset.newDataAttr = 'New Attr'
+// console.log(helloSpan.dataset)
+
+// helloSpan.classList.add('hi')
+// helloSpan.classList.remove('hi')
+
+// helloSpan.classList.toggle('hi')
+// helloSpan.classList.toggle('hi')
+
+helloSpan.style.backgroundColor = 'red'
+
